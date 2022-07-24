2540afdc95da128820282512c1476eaba9d6a20b
Grégory Marini <greg.marini@hotmail.fr>
2022-07-24 19:10:39 +0200

    creation de la vue du récap panier avec la logic de calcul, à terminer quelques options(augmenter quantité, icon panier, vider le panier...)

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,19 +3,35 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/mon-panier', name: 'app_cart')]
-    public function index(RequestStack $stack): Response
+
+    public function __construct(private EntityManagerInterface $em)
     {
         
-        dd($stack->getSession()->get('cart'));
-        return $this->render('cart/index.html.twig');
+    }
+
+    #[Route('/mon-panier', name: 'app_cart')]
+    public function index(Cart $cart): Response
+    {
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $this->em->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];            
+        }
+                
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'app_add_to_cart')]
