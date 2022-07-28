5a7d168142535bed313d42d5b01c56dc2be2b51a
Grégory Marini <greg.marini@hotmail.fr>
2022-07-28 19:50:36 +0200

    début du tunnel d'achat, création formulaire order avec sélection de l'adresse de livraison, modif sur AddressType rajout de required false au champ 'company',modif stylisation de la page inscription

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -30,6 +30,7 @@ class AddressType extends AbstractType
             ])
             ->add('company', TextType::class, [
                 'label' => 'Votre société :',
+                'required' => false,
                 'attr' => ['placeholder' => '(facultatif) Entrez le nom de votre société']
             ])
             ->add('address', TextType::class, [
