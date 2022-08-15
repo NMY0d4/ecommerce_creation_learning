3ad5a76b316cfdba301a5988706e6117dc45334c
Grégory Marini <greg.marini@hotmail.fr>
2022-08-15 15:37:57 +0200

    Stockage des informations de la commande en base de données(création OrderController), ajout des vue nécessaire et stylisation(order/aadd et order/index)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +16,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+
+    public function __construct(private EntityManagerInterface $em)
+    {
+        
+    }
+
     #[Route('/commande', name: 'app_order')]
     public function index(Cart $cart, Request $request): Response
     {
@@ -21,17 +31,71 @@ class OrderController extends AbstractController
 
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
-        ]);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
-        }
+        ]);        
 
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
             'cart' => $cart->getFull()
         ]);
     }
+
+    #[Route('/commande/recapitulatif', name: 'app_order_recap', methods:['POST'])]
+    public function add(Cart $cart, Request $request): Response
+    {        
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new DateTimeImmutable();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();  
+            $delivery_content.= '<br/>'.$delivery->getPhone();
+                     
+            if($delivery->getCompany()) $delivery_content.= '<br/>'.$delivery->getCompany();
+
+            $delivery_content.= '<br/>'.$delivery->getAddress();
+            $delivery_content.= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
+            $delivery_content.= ' - '.$delivery->getCountry();
+
+            // Enregistrer ma commande order
+            $order = new Order;
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            $this->em->persist($order);
+
+            // Enregistrer mes produits orderderails            
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails;
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']); 
+                $this->em->persist($orderDetails);            
+            }
+
+            // $this->em->flush();
+
+            return $this->render('order/add.html.twig', [            
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers, 
+                'delivery' => $delivery_content
+            ]);
+
+        }
+
+        return $this->redirectToRoute('app_cart');
+        
+    }
+
 }
