1da497aa63ee68de932a7a1f5f4ef249d2b972b4
Grégory Marini <greg.marini@hotmail.fr>
2022-08-23 18:32:47 +0200

    mis en fonction du lien mes commandes à partir du compte utilisateur(uniquement pour les commandes isPaid -> création d'un find dans le orderRepo)

---
+++ src/Controller/AccountOrderController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountOrderController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+        
+    }
+
+    #[Route('/compte/mes-commandes', name: 'app_account_order')]
+    public function index(): Response
+    {
+        $orders = $this->em->getRepository(Order::class)->findSuccessOrders($this->getUser());
+
+        
+
+        return $this->render('account/order.html.twig', compact('orders'));
+    }
+
+    #[Route('/compte/mes-commandes/{reference}', name: 'app_account_order_show')]
+    public function show($reference): Response
+    {
+        $order = $this->em->getRepository(Order::class)->findOneByReference($reference);
+
+        if(!$order || $order->getUser() != $this->getUser()){
+            return $this->redirectToRoute('app_account_order');
+        }
+        
+
+        return $this->render('account/order_show.html.twig', compact('order'));
+    }
+}
